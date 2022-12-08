a7d77fad35a15da705130643a1584a325dd913c8
ArieleMartins <ariele.cmmelo@gmail.com>
2022-12-08 16:38:29 -0300

    :tada: New project - FAQ accordion component :tada:

---
+++ front_end/FAQ-accordion-card/assets/js/script.js
@@ -0,0 +1,21 @@
+const lists = document.querySelectorAll('li')
+
+window.addEventListener('load', checkEvent(false, ''))
+
+function checkEvent(boolean, element){
+    lists.forEach(list =>{
+        if(boolean){
+            if(list.classList.contains('active')){
+                list.classList.remove('active')
+            }else if(!list.classList.contains('active') && list.attributes[0].nodeValue == element){
+                list.classList.add('active')
+            }
+        }else{
+            lists.forEach(list =>{
+                list.addEventListener('click', () =>{checkEvent(true, list.attributes[0].nodeValue)})
+            })
+        }
+    })
+    
+    
+}
---
+++ front_end/FAQ-accordion-card/readme.md
@@ -0,0 +1,15 @@
+<h1 align="center">FAQ ACCORDION COMPONENT</h1>
+<p align="center"> Desafio de desenvolver layout proposto pelo <a href="https://www.frontendmentor.io/home" target="_blank">FrontEnd Mentor</a></p>
+
+![faq_accordion_component](https://user-images.githubusercontent.com/83427685/206551097-db94efff-8035-4b8f-a20b-2a282e7be132.png)
+
+<h1 align="center">Tecnologias</h1>
+
+- HTML
+- CSS
+
+<h1 align="center">DESENVOLVEDORES</h1>
+<div align="center">
+    <img style="border-radius: 50%" height="200em" src="https://github.com/ArieleMartins.png">
+    <h2 >Ariele Martins</h2>
+</div>
