fd0bc0bacb3a70c6250a0e5a2d89d6602c5c0984
ArieleMartins <ariele.cmmelo@gmail.com>
2022-12-22 12:40:09 -0300

    Add event menu esports

--- front_end/blizzard-brChallege/assets/js/menugames.js
+++ front_end/blizzard-brChallege/assets/js/menugames.js
@@ -1,83 +1,173 @@
 const menuGames = document.getElementById('menuGames')
+const menuEsports = document.getElementById('menuEsports')
+const header = document.querySelector(".header")
+const ulList = document.querySelector('.list')
+const ulFooter = document.querySelector('.list-links')
 
 var activeMenuGames = false
-
-const listGames = [
-    {
-        name: "Diablo® II ressurected",
-        image: "Diablo 2.svg"
-    },
-    {
-        name: "Overwatch® 2",
-        image: "Overwatch2.svg"
-    },
-    {
-        name: "World of Warcraft®",
-        image: "WorldOfWarcraft.svg"
-    },
-    {
-        name: "Hearthstone®",
-        image: "Hearthstone.svg"
-    },
-    {
-        name: "Heroes of the storm®",
-        image: "HeroesOfTheStorm.svg"
-    },
-    {
-        name: "Warcraft® III Reforged",
-        image: "WarcraftIIIReforged.svg"
-    },
-    {
-        name: "Diablo® IV",
-        image: "Diablo IV.svg"
-    },
-    {
-        name: "Diablo® Immortal",
-        image: "DiabloImmortal.svg"
-    },
-    {
-        name: "Diablo® III",
-        image: "DiabloIII.svg"
-    },
-    {
-        name: "StarCraft® II",
-        image: "StarCraft.svg"
-    },
-    {
-        name: "StarCraft® Remastered",
-        image: "StarCraftRemastered.svg"
-    },
-    {
-        name: "Arcade da Blizzard®",
-        image: "ArcadeDaBlizzard.svg"
-    }
-]
+var activeMenuEsports = false
 
 menuGames.addEventListener('click', showGames)
+menuEsports.addEventListener('click', showEsports)
 
 function showGames(){
-    const header = document.querySelector(".header")
-
-    const ulGames = document.querySelector('.list')
+    
+    ulList.innerHTML = ""
+    ulFooter.innerHTML = ""
     activeMenuGames = !activeMenuGames
 
     if(activeMenuGames){
         header.classList.add('active')
         header.classList.remove('disabled')
         menuGames.children[0].classList.add('open-menu')
-        listGames.forEach(game =>{
-            ulGames.innerHTML += `
-                <li>
-                    <img src="assets/images/banner-hero/icons/icon-games/${game.image}" alt="${game.name}"/>
-                    <span>${game.name}</span>
-                </li>
-            `
-        })
+        showListGames()
     }else{
-        ulGames.innerHTML = ''
         menuGames.children[0].classList.remove('open-menu')
         header.classList.remove('active')
         header.classList.add('disabled')
     }
     
 }
+
+function showListGames(){
+    const listGames = [
+        {
+            name: "Diablo® II ressurected",
+            image: "Diablo 2.svg"
+        },
+        {
+            name: "Overwatch® 2",
+            image: "Overwatch2.svg"
+        },
+        {
+            name: "World of Warcraft®",
+            image: "WorldOfWarcraft.svg"
+        },
+        {
+            name: "Hearthstone®",
+            image: "Hearthstone.svg"
+        },
+        {
+            name: "Heroes of the storm®",
+            image: "HeroesOfTheStorm.svg"
+        },
+        {
+            name: "Warcraft® III Reforged",
+            image: "WarcraftIIIReforged.svg"
+        },
+        {
+            name: "Diablo® IV",
+            image: "Diablo IV.svg"
+        },
+        {
+            name: "Diablo® Immortal",
+            image: "DiabloImmortal.svg"
+        },
+        {
+            name: "Diablo® III",
+            image: "DiabloIII.svg"
+        },
+        {
+            name: "StarCraft® II",
+            image: "StarCraft.svg"
+        },
+        {
+            name: "StarCraft® Remastered",
+            image: "StarCraftRemastered.svg"
+        },
+        {
+            name: "Arcade da Blizzard®",
+            image: "ArcadeDaBlizzard.svg"
+        }
+    ]
+    
+    const listGameFooter = [
+        {
+            icon: "mais.svg",
+            text: "Ver todos os jogos"
+        },
+        {
+            icon: "battle.svg",
+            text: "Aplicativo Battle.net"
+        },
+        {
+            icon: "downloads.svg",
+            text: "Downloads"
+        },
+        {
+            icon: 'forum.svg',
+            text: "Fóruns dos jogos"
+        }
+    ]
+
+    listGames.forEach(game =>{
+        ulList.innerHTML += `
+            <li>
+                <img src="assets/images/banner-hero/icons/icon-games/${game.image}" alt="${game.name}"/>
+                <span>${game.name}</span>
+            </li>
+        `
+    })
+
+    listGameFooter.forEach(link =>{
+        ulFooter.innerHTML += `
+        <li>
+            <img src="assets/images/banner-hero/icons/${link.icon}" />
+            <span>${link.text}</span>
+        </li>
+        `
+    })
+}
+
+function showEsports(){
+    activeMenuEsports = !activeMenuEsports
+    ulList.innerHTML = ""
+    ulFooter.innerHTML = ""
+
+    if(activeMenuEsports){
+        header.classList.add('active')
+        header.classList.remove('disabled')
+        menuGames.children[0].classList.add('open-menu')
+        showListEsports()
+    }else{
+        menuGames.children[0].classList.remove('open-menu')
+        header.classList.remove('active')
+        header.classList.add('disabled')
+    }
+}
+
+function showListEsports(){
+    var listEsports = [
+        {
+            image: "HearthstoneMasters.svg",
+            text: "Hearthstone® masters"
+        },
+        {
+            image: "CampeonatoMundialDeArenaWoW.svg",
+            text: 'Campeonato Mundial de Arena WoW®'
+        },
+        {
+            image: "StarCraftIIWCS.svg",
+            text: "StarCraft® II WCS"
+        },
+        {
+            image: "CopaMundialDeOverwatch.svg",
+            text: "Copa Mundial de Overwatch®"
+        },
+        {
+            image: "LigaDeOverwatch.svg",
+            text: "Liga de Overwatch®"
+        }
+    ]
+
+    listEsports.forEach(esport =>{
+        ulList.innerHTML += `
+        <li>
+            <img src="assets/images/banner-hero/icons/icon-esports/${esport.image}" />
+            <span>${esport.text}</span>
+        </li>
+        `
+    })
+}
+
+
