6efc36fd732d66f93076517b83e35a691fecc2e8
ArieleMartins <ariele.cmmelo@gmail.com>
2022-12-21 21:50:59 -0300

    Add style header

---
+++ front_end/blizzard-brChallege/assets/js/menugames.js
@@ -0,0 +1,82 @@
+const menuGames = document.getElementById('menuGames')
+
+var activeMenuGames = false
+
+const listGames = [
+    {
+        name: "Diablo® II ressurected",
+        image: "Diablo 2.svg"
+    },
+    {
+        name: "Overwatch® 2",
+        image: "Overwatch2.svg"
+    },
+    {
+        name: "World of Warcraft®",
+        image: "WorldOfWarcraft.svg"
+    },
+    {
+        name: "Hearthstone®",
+        image: "Hearthstone.svg"
+    },
+    {
+        name: "Heroes of the storm®",
+        image: "HeroesOfTheStorm.svg"
+    },
+    {
+        name: "Warcraft® III Reforged",
+        image: "WarcraftIIIReforged.svg"
+    },
+    {
+        name: "Diablo® IV",
+        image: "Diablo IV.svg"
+    },
+    {
+        name: "Diablo® Immortal",
+        image: "DiabloImmortal.svg"
+    },
+    {
+        name: "Diablo® III",
+        image: "DiabloIII.svg"
+    },
+    {
+        name: "StarCraft® II",
+        image: "StarCraft.svg"
+    },
+    {
+        name: "StarCraft® Remastered",
+        image: "StarCraftRemastered.svg"
+    },
+    {
+        name: "Arcade da Blizzard®",
+        image: "ArcadeDaBlizzard.svg"
+    }
+]
+
+menuGames.addEventListener('click', showGames)
+
+function showGames(){
+    const header = document.querySelector(".header")
+
+    const ulGames = document.querySelector('.list')
+    activeMenuGames = !activeMenuGames
+
+    if(activeMenuGames){
+        header.classList.add('active')
+        header.classList.remove('disabled')
+        listGames.forEach(game =>{
+            ulGames.innerHTML += `
+                <img src="assets/images/banner-hero/icons/icon-games/${game.image}" alt="${game.name}"/>
+                <span>${game.name}</span>
+            `
+        })
+    }else{
+        setTimeout(()=>{
+            ulGames.innerHTML = ''
+        }, 700)
+        
+        header.classList.remove('active')
+        header.classList.add('disabled')
+    }
+    
+}
